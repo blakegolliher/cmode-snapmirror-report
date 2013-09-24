7cadd6afa3850f5d0fd978f95d41d9cc4e0e5396
Blake Golliher <blakegolliher@gmail.com>
2013-09-23 17:27:05 -0700

    Update cmode-snapmirror-report.py
    Fixed up logic, made cleaner.  Added lag for filers running 8.2.

--- cmode-snapmirror-report.py
+++ cmode-snapmirror-report.py
@@ -10,26 +10,10 @@ import sys, string, os
 import getpass
 import datetime
 import time
-from math import log
+from datetime import datetime, timedelta
 
 sys.path.append("/var/local/netapp-manageability-sdk-5.1/lib/python/NetApp")
 from NaServer import *
-
-## Thanks internet!
-## http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
-unit_list = zip(['bytes', 'kB', 'MB', 'GB', 'TB'], [0, 0, 1, 2, 2])
-def sizeof_fmt(num):
-    """Human friendly file size"""
-    if num > 1:
-        exponent = min(int(log(num, 1024)), len(unit_list) - 1)
-        quotient = float(num) / 1024**exponent
-        unit, num_decimals = unit_list[exponent]
-        format_string = '{:.%sf} {}' % (num_decimals)
-        return format_string.format(quotient, unit)
-    if num == 0:
-        return '0 bytes'
-    if num == 1:
-        return '1 byte'
 
 password = getpass.getpass()
 
@@ -41,7 +25,7 @@ cmd = NaElement("snapmirror-get-iter")
 ret = filer.invoke_elem(cmd)
 
 if(ret.results_status() == "failed"):
-  print "%s failed." % filer_name
+  	print "%s failed." % filer_name
 	print(ret.results_reason() + "\n")
 	sys.exit(2)
 
@@ -54,17 +38,15 @@ smlist = dict()
 smlist = ret.child_get('attributes-list')
 
 print "\nData Protection Snapmirror Relationships for %s : " % filer_name
-
 for mirror in smlist.children_get():
-	if(mirror.child_get_string("relationship-progress") == None):
-		rate = 0
-		if(mirror.child_get_string("relationship-type") == "data_protection"):
-			print "	%s -> %s : %s : Transferred %s so far. " % (mirror.child_get_string("source-location"),mirror.child_get_string("destination-location"),mirror.child_get_string("relationship-status"),sizeof_fmt(int(rate)))
-			print "	State is %s" % mirror.child_get_string("mirror-state")
-			print "	Snapshot in flight is %s" % mirror.child_get_string("transfer-snapshot")
-	else:
+	type = mirror.child_get_string("relationship-type")
+	if type == "data_protection":
+		type = mirror.child_get_string("relationship-type")
 		rate = mirror.child_get_string("relationship-progress")
-		if(mirror.child_get_string("relationship-type") == "data_protection"):
-			print "	%s -> %s : %s : Transferred %s so far. " % (mirror.child_get_string("source-location"),mirror.child_get_string("destination-location"),mirror.child_get_string("relationship-status"),sizeof_fmt(int(rate)))
-			print "	State is %s" % mirror.child_get_string("mirror-state")
-			print "	Snapshot in flight is %s" % mirror.child_get_string("transfer-snapshot")
+		sourcelocation = mirror.child_get_string("source-location")
+		destlocation = mirror.child_get_string("destination-location")
+		print "	%s -> %s : %s : Transferred %s so far. " % (sourcelocation,destlocation,type,rate)
+		print "	State is %s" % mirror.child_get_string("mirror-state")
+		print "	Type is	: %s" % type
+		print "	Snapshot in flight is %s" % mirror.child_get_string("transfer-snapshot")
+		print "	Snapmirror lag is %s" % mirror.child_get_string("lag-time")
